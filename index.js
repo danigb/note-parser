a2c2fa0e909b58c50207d593f91819b912b75ea7
danigb <danigb@gmail.com>
2015-05-18 20:38:56 +0200

    First version

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 'use strict';
 
-var NOTE = /^([a-gA-G])(\d{0,1)$/
+var NOTE = /^([a-gA-G])(#{0,2}|b{0,2})(-?\d{0,1})$/
+/*
+ * parseNote
+ *
+ * @param {String} note - the note string to be parsed
+ * @return {Object} a object with the following attributes:
+ * - pitchClass: the letter of the note, ALWAYS in lower case
+ * - accidentals: the accidentals (or '' if no accidentals)
+ * - octave: the octave as integer. If not present in the string, its 2
+ */
 module.exports = function(note) {
   var match = NOTE.exec(note);
   if(match) {
-    return { pitchClass: match[1].toUpperCase(), acc: match[2], octave: octave };
+    var octave = match[3] !== '' ? +match[3] : 2;
+    return { pitchClass: match[1].toLowerCase(), accidentals: match[2], octave: octave };
   } else {
     return null;
   }
--- test/parse.js
+++ test/parse.js
@@ -4,8 +4,33 @@ assert = require('assert');
 var parse = require('..');
 
 vows.describe('Note parse').addBatch({
-  "parse notes": function() {
-    assert.equal(parse('c2'), 's');
+  "parse pitchClass": function() {
+    assert.equal(parse('c').pitchClass, 'c');
+    assert.equal(parse('C').pitchClass, 'c');
+  },
+  "parse accidentals": function() {
+    assert.equal(parse('c').accidentals, '');
+    assert.equal(parse('c#').accidentals, '#');
+    assert.equal(parse('c##').accidentals, '##');
+    assert.equal(parse('cb').accidentals, 'b');
+    assert.equal(parse('cbb').accidentals, 'bb');
+  },
+  "parse octave": function() {
+    assert.equal(parse('c').octave, 2);
+    assert.equal(parse('c3').octave, 3);
+    assert.equal(parse('c-1').octave, -1);
+    assert.equal(parse('c-2').octave, -2);
+  },
+  "invalid pitchClass": function() {
+    assert.equal(parse('i'), null);
+  },
+  "invalid accidentals": function() {
+    assert.equal(parse('c###'), null);
+    assert.equal(parse('cbbb'), null);
+    assert.equal(parse('c#b'), null);
+    assert.equal(parse('cb#'), null);
+  },
+  "invalid octave": function() {
+    assert.equal(parse('c11'), null);
   }
-
 }).export(module);
