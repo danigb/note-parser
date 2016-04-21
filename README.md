b6d7d6a5a7ff0c680c16fa4c2a401cedc2bdd138
danigb <danigb@gmail.com>
2016-04-21 11:51:05 +0200

    1.0.0

--- README.md
+++ README.md
@@ -3,61 +3,22 @@
 [![Code Climate](https://codeclimate.com/github/danigb/note-parser/badges/gpa.svg)](https://codeclimate.com/github/danigb/note-parser)
 [![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](https://github.com/feross/standard)
 
-Parse notes with javascript. Fast and simple: give it a string, obtain a hash
-with note's pitchClass, accidentals, octave, midi number and frequency.
+Parse note names (in [scientific notation](https://en.wikipedia.org/wiki/Scientific_pitch_notation)) with javascript. Fast and simple: give it a string, obtain a hash
+with note properties (including midi number and frequency)
 
 ## Usage
 
-Add the module to your project: `npm i --save note-parser` and require it:
-```js
-var parse = require('note-parser');
-```
-
-#### parse(noteString [, defaultOctave, defaultValue ])
-
-Use the function to parse notes:
+Install via npm: `npm i --save note-parser` and require it:
 
 ```js
-parse('Db4');   // => { name: 'db4',   pc: 'd', acc: 'b',  oct: 4,  midi: 61, freq: 277.18 }
-parse('f##-2'); // => { name: 'f##-2', pc: 'f', acc: '##', oct: -2, midi: -5, freq: 6.12 }
-parse('Eb++');  // => { name: 'eb6',   pc: 'e', acc: 'b',  oct: 6,  midi: 87, freq: 1244.50 }
-parse('b#-');   // => { name: 'b#3',   pc: 'b', acc: '#',  oct: 3,  midi: 60, freq: 261.62 }
-parse('g');     // => { name: 'g4',    pc: 'g', acc: '',   oct: 4,  midi: 67, freq: 391.99 }
+var parser = require('note-parser')
+parser.parse('c#4') // => { letter: 'C', acc: '#', ... midi: 61, freq: 277.1826309768721 }
 ```
 
-The parse method receives a string and return an object with the following
-attributes:
-- name: the scientific name of the note. Always in lowercase
-- pc: pitchClass, the _letter_ of the note. From `"a"` to `"g"`. __Always__ in lowercase.
-- acc: a string with the accidentals. An empty string if no accidentals present.
-- oct: the octave as integer.
-- midi: the midi number
-- freq: the note frequency
+The following properties are returned:
 
-You can change the default octave with the second parameter. Otherwise is 4:
 
-```js
-parse('C');      // => { name: 'c4', pc: 'c', acc: '', oct: 4, midi: 60, freq: 261.63 }
-parse('C', 2);   // => { name: 'c2', pc: 'c', acc: '', oct: 2, midi: 36, freq: 65.41 }
-parse('C+', 2);  // => { name: 'c3', pc: 'c', acc: '', oct: 3, midi: 48, freq: 130.81 }
-parse('C--', 2); // => { name: 'c0', pc: 'c', acc: '', oct: 0, midi: 12, freq: 16.35 }
-```
-
-If defaultValue is not defined, the parse throws an exception if the note format is invalid.
-Otherwise returns the defaultValue:
-
-```js
-parse('blah'); // => throws Error
-parse('blah', 2, parse('C4')); // => { name: 'c4', pc: 'c', acc: '', oct: 4 ... }
-parse('blah', 2, null); // => null
-```
-
-Note: _calling parse on a parsed object return itself_:
-
-```js
-var n = parse('C');
-n === parse(n) // => true
-```
+## Tests and documentation
 
 ## License
 
---
+++ test/note-parser-test.js
@@ -0,0 +1,130 @@
+/* global describe it */
+
+var assert = require('assert')
+var parser = require('..')
+
+function map (fn) {
+  return function (str) {
+    return str.split(' ').map(fn)
+  }
+}
+
+describe('note parser', function () {
+  describe('parse', function () {
+    var parse = parser.parse
+    it('parse pitch classes', function () {
+      assert.deepEqual(parse('fx'),
+        { letter: 'F', acc: '##', pc: 'F##', step: 3, alt: 2, chroma: 7 })
+    })
+    it('parse notes', function () {
+      assert.deepEqual(parse('Cb4'),
+      { letter: 'C', acc: 'b', pc: 'Cb', step: 0, alt: -1, chroma: -1,
+        oct: 4, midi: 59, freq: 246.94165062806206 })
+      assert.deepEqual(parse('A#4'),
+      { letter: 'A', acc: '#', pc: 'A#', step: 5, alt: 1, chroma: 10,
+        oct: 4, midi: 70, freq: 466.1637615180899 })
+    })
+    it('parse tonicOf', function () {
+      assert.equal(parse('CMaj7', true).tonicOf, 'Maj7')
+      assert.equal(parse('C4 major', true).tonicOf, 'major')
+      assert.equal(parse('C64', true).tonicOf, '')
+      assert.equal(parse('G7', true).tonicOf, '')
+    })
+    it('different tunning', function () {
+      assert.equal(parse('A2', false, 444).freq, 111)
+    })
+  })
+  describe('regex', function () {
+    it('regex return a RegExp object', function () {
+      assert(parser.regex() instanceof RegExp)
+    })
+  })
+  describe('letter', function () {
+    var letters = map(parser.letter)
+    it('get note letters', function () {
+      assert.deepEqual(letters('a b c d e f g'),
+        [ 'A', 'B', 'C', 'D', 'E', 'F', 'G' ])
+    })
+  })
+  describe('acc', function () {
+    var accs = map(parser.acc)
+    it('get note accidentals', function () {
+      assert.deepEqual(accs('c d e f g a b'), [ '', '', '', '', '', '', '' ])
+      assert.deepEqual(accs('c# d# e# f# g# a# b#'), [ '#', '#', '#', '#', '#', '#', '#' ])
+      assert.deepEqual(accs('c## d## e## f## g## a## b##'), [ '##', '##', '##', '##', '##', '##', '##' ])
+      assert.deepEqual(accs('cx dx ex fx gx ax bx'), [ '##', '##', '##', '##', '##', '##', '##' ])
+      assert.deepEqual(accs('cb db eb fb gb ab bb'), [ 'b', 'b', 'b', 'b', 'b', 'b', 'b' ])
+      assert.deepEqual(accs('cbb dbb ebb fbb gbb abb bbb'), [ 'bb', 'bb', 'bb', 'bb', 'bb', 'bb', 'bb' ])
+    })
+  })
+  describe('pc', function () {
+    var pcs = map(parser.pc)
+    it('get pitch classes', function () {
+      assert.deepEqual(pcs('c d# e## fx gb abb bbb'),
+        [ 'C', 'D#', 'E##', 'F##', 'Gb', 'Abb', 'Bbb' ])
+    })
+  })
+  describe('steps', function () {
+    var steps = map(parser.step)
+    it('get note steps', function () {
+      assert.deepEqual(steps('c d e f g a b'),
+        [ 0, 1, 2, 3, 4, 5, 6 ])
+    })
+  })
+  describe('alt', function () {
+    var alts = map(parser.alt)
+    it('get alteration', function () {
+      assert.deepEqual(alts('c d# e## fx gb abb bbb'),
+        [ 0, 1, 2, 2, -1, -2, -2 ])
+    })
+  })
+  describe('chroma', function () {
+    var chromas = map(parser.chroma)
+    it('get chroma', function () {
+      assert.deepEqual(chromas('cb c db d eb e fb f gb g ab a bb b'),
+        [ -1, 0, 1, 2, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11 ])
+      assert.deepEqual(chromas('c c# d d# e e# f f# g g# a a# b b#'),
+        [ 0, 1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10, 11, 12 ])
+    })
+  })
+  describe('oct', function () {
+    it('no octaves for pitch classes', function () {
+      assert(parser.oct('c') === null)
+    })
+    it('no max octaves', function () {
+      assert.deepEqual(parser.oct('C9999'), 9999)
+    })
+    it('no min octaves', function () {
+      assert.deepEqual(parser.oct('C-9999'), -9999)
+    })
+    it('get octaves', function () {
+      var octs = map(parser.oct)
+      assert.deepEqual(octs('c-1 d0 e1 f2 g3 a4 b5'),
+        [ -1, 0, 1, 2, 3, 4, 5 ])
+    })
+  })
+  describe('midi', function () {
+    it('no midi for pitch classes', function () {
+      assert(parser.midi('c') === null)
+    })
+    it('follows general midi number', function () {
+      assert.equal(parser.midi('c-1'), 0)
+    })
+    it('get midis', function () {
+      var midis = map(parser.midi)
+      assert.deepEqual(midis('c4 d4 e4 f4 g4 a4 b4'),
+        [ 60, 62, 64, 65, 67, 69, 71 ])
+    })
+  })
+  describe('freq', function () {
+    it('no freq for pitch classes', function () {
+      assert.equal(parser.freq('c'))
+    })
+    it('get frequencies', function () {
+      var freqs = map(parser.freq)
+      assert.deepEqual(freqs('c4 d4 e4 f4 g4 a4 b4'),
+      [ 261.6255653005986, 293.6647679174076, 329.6275569128699,
+        349.2282314330039, 391.99543598174927, 440, 493.8833012561241])
+    })
+  })
+})
